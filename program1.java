089f64a30d6db545db3f88154256fd98feaf2eea
jathin <jathinyendamuri@gmail.com>
2023-08-18 23:52:02 +0530

    my hava program

---
+++ program1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Demo
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in)
+		System.out.println("enetr the first value");
+		int a = sc.nextInt();
+	        System.out.println("enter the second value");
+		int b = sc.nextInt();
+		System.out.println("the sum of the two numbers is :"+(a+b))
+	}
+}
+
